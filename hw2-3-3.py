7f99c2c498387b0faa6a404c6f56f8371d0fcb05
p22mgiroux <77790842+p22mgiroux@users.noreply.github.com>
2021-09-17 11:15:45 -0400

    Finish HW 2-3-3

--- hw2-3-3.py
+++ hw2-3-3.py
@@ -1,1 +1,10 @@
 # Author MOG 9/17/21
+
+rectangle_w = 13.9
+rectangle_h = 8.2
+
+rectangle_perim = (rectangle_w * 2) + (rectangle_h * 2)
+rectangle_area = rectangle_w * rectangle_h
+
+print("The perimeter of a rectangle with a width of {} and a height of {}, is {}.".format(rectangle_w, rectangle_h, rectangle_perim))
+print("The area of a rectangle with a width of {} and a height of {}, is {}.".format(rectangle_w, rectangle_h, rectangle_area))

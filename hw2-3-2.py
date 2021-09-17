61057d5b32716b6ec900f04c39de1cc08eeeda71
p22mgiroux <77790842+p22mgiroux@users.noreply.github.com>
2021-09-17 11:07:14 -0400

    Finish HW 2-3-2

--- hw2-3-2.py
+++ hw2-3-2.py
@@ -1,1 +1,10 @@
 # Author MOG 9/17/21
+
+circle_rad = 4.6
+pi = 3.141
+
+circle_perim = 2 * pi * circle_rad
+circle_area = pi * (circle_rad ** 2)
+
+print("The perimeter of a circle with a radius of {}, is {}.".format(circle_rad, circle_perim))
+print("The area of a circle with a radius of {}, is {}.".format(circle_rad, circle_area))
